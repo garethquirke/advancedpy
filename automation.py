491286142d0dd926b2c8bed7ec5ea6f36ebef130
Gareth Quirke <gareth.quirke@gmail.com>
2017-09-25 11:30:22 +0100

    automation and spider multi proccessing updated

--- automation.py
+++ automation.py
@@ -1,2 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\chromedriver_win32\chromedriver.exe')
+
+driver.get('http://www.github.com/garethquirke/')
+driver.close()
--- spider-multiproccessing.py
+++ spider-multiproccessing.py
@@ -11,4 +11,51 @@ def random_starting_url():
     url = ''.join(['http://', starting, '.com'])
     return url
 
-# to do : finish at home without proxy
+
+def handle_local_links(url,link):
+    if link.startswith('/'):
+        return ''.join([url,link])
+    else:
+        return link
+
+def get_links(url):
+    try:
+        resp = requests.get(url)
+        soup = bs.BeautifulSoup(resp.text, 'lxml')
+        body = soup.body
+        links = [link.get('href') for link in body.find_all('a')]
+        links = [handle_local_links(url,link) for link in links]
+        links = [str(link.encode("ascii")) for link in links]
+        return links
+
+    except TypeError as e:
+        print(e)
+        print('Got a TypeError, probably got a None that we tried to iterate over')
+        return []
+    except IndexError as e:
+        print(e)
+        print('We probably did not find any useful links, returning empty list')
+        return []
+    except AttributeError as e:
+        print(e)
+        print('Likely got None for links, so we are throwing this')
+        return []
+    except Exception as e:
+        print(str(e))
+        # log this error 
+        return []
+
+def main():
+    how_many = 50
+    p = Pool(processes=how_many)
+    parse_us = [random_starting_url() for _ in range(how_many)]
+    
+    data = p.map(get_links, [link for link in parse_us])
+    data = [url for url_list in data for url in url_list]
+    p.close()
+
+    with open('urls.txt','w') as f:
+        f.write(str(data))
+
+if __name__ == '__main__':
+    main()
